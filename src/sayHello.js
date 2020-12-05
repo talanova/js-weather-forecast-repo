e71bdc1c4bed8ae2762062e5f837486e28e6b713
talanova <talanova@speechpro.com>
2020-12-05 22:10:49 +0300

    config jest and do some checks

--- src/sayHello.js
+++ src/sayHello.js
@@ -1,3 +1,3 @@
 export function sayHello() {
-  alert("Hello!");
+  global.alert("Hello!");
 }
--- src/sayHello.test.js
+++ src/sayHello.test.js
@@ -4,4 +4,11 @@ describe("sayHello", () => {
   it("is a function", () => {
     expect(typeof sayHello).toBe("function");
   });
+
+  it("says hello", () => {
+    global.alert = jest.fn();
+    jest.spyOn(global, "alert");
+    sayHello();
+    expect(global.alert).toHaveBeenCalledWith("Hello!");
+  });
 });

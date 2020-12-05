0760c5ce1c83d85830d230b41f57bcfa6ac82c02
talanova <talanova@speechpro.com>
2020-12-05 21:04:48 +0300

    setup jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,19 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true,
   },
-  extends: ['airbnb-base'],
+  extends: ["airbnb-base", "plugin:jest/recommended", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  rules: {},
+  rules: {
+    "import/prefer-default-export": "off",
+    "jest/valid-title": "off",
+    "no-console": "off",
+    "no-alert": "off",
+    "no-restricted-globals": "off",
+    "no-plusplus": "off",
+  },
 };

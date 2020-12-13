939177523f36a150403136a018b3cb951cccb890
talanova <talanova@speechpro.com>
2020-12-13 17:34:32 +0300

    do some manupulations on babel

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,13 @@
 // babel.config.js
 module.exports = {
-  presets: [["@babel/preset-env", { targets: "defaults" }]],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: { node: "current" },
+        useBuiltIns: "entry", // alternative mode: "entry"/ "usage"
+        corejs: 3, // default would be 2
+      },
+    ],
+  ],
 };

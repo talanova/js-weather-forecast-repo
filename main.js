5999117da9029cc07ad04deb5e6bc9838cd44720
talanova <51947887+talanova@users.noreply.github.com>
2021-01-10 15:51:18 +0000

    Deploying to gh-pages from  @ 1aa7dba3dd35bf2b1c5ff42b1f245c4c9abc59ca 🚀

--- main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";var t={};t.g=function(){if("object"==typeof globalThis)return globalThis;try{return this||new Function("return this")()}catch(t){if("object"==typeof window)return window}}(),t.g.alert("Hello!")})();

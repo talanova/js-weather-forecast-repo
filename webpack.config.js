bedbe2d84eb59a10290cf614de4c12aee5cc525d
talanova <talanova@speechpro.com>
2021-03-24 10:02:18 +0300

    do some updates before start

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require("webpack");
 const path = require("path");
 
@@ -9,7 +11,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: "[name].bundle.js",
+    filename: "[name].js",
   },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
@@ -19,7 +21,33 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({ template: "index.html" }),
+    new MiniCssExtractPlugin({
+      filename: "./style.css",
+    }),
     new CleanWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    new BrowserSyncPlugin(
+      {
+        host: "localhost",
+        port: 3000,
+        proxy: "http://localhost:9000/",
+      },
+      {
+        reload: false,
+      }
+    ),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };

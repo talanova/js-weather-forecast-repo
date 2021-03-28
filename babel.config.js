6e52edebedcf813d8d9b20d08dcf9769f3964dc9
talanova <talanova@speechpro.com>
2021-03-28 20:32:06 +0300

    hotfix

--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,7 @@ module.exports = {
         useBuiltIns: "entry", // alternative mode: "entry"/ "usage"
         corejs: 3, // default would be 2
       },
-      "@babel/preset-typescript",
     ],
+    "@babel/preset-typescript",
   ],
 };

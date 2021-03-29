debd9a495cf65c48de3855976c368e28ed413117
talanova <talanova@speechpro.com>
2021-03-29 20:18:19 +0300

    refactoring

--- src/template.ts
+++ src/template.ts
@@ -1,60 +1,72 @@
+function loopTemplate(tpl: string, data: any): string {
+  const re = /{{for ([^}]+)}}((?:\s|\S)+){{endfor}}/gi;
+  return tpl.replace(re, (match, param, body) => {
+    if (data[param]) {
+      return data[param]
+        .map((el: string, index: number) =>
+          // eslint-disable-next-line no-use-before-define
+          internal(body, el, index, data[param].length)
+        )
+        .join("");
+    }
+    return "";
+  });
+}
+
+function ifTemplate(
+  tpl: string,
+  data: any,
+  indexInLoop: number = -1,
+  loopLength: number = -1
+): string {
+  const re = /{{if ([^}]+)}}((?:\s|\S)+){{endif}}/gi;
+
+  return tpl.replace(re, (match, param, body) => {
+    const arr: string[] = body.split("{{else}}");
+
+    if (param in data) {
+      if (data[param]) {
+        return arr[0];
+      }
+      if (arr.length === 2) {
+        return arr[1];
+      }
+      return "";
+    } else if (param === "notIsLastElement") {
+      if (indexInLoop !== loopLength - 1) {
+        return arr[0];
+      } else if (arr.length === 2) {
+        return arr[1];
+      }
+      return "";
+    } else if (param === "isLastElement") {
+      if (indexInLoop === loopLength - 1) {
+        return arr[0];
+      } else if (arr.length === 2) {
+        return arr[1];
+      }
+      return "";
+    }
+    return "";
+  });
+}
+
+function keyTemplate(tpl: string, data: any): string {
+  const re = /{{([^}]+)}}/gi;
+  return tpl.replace(re, (match, param) => {
+    return data[param] ? data[param] : "";
+  });
+}
+
 function internal(
   tpl: string,
   data: any,
   indexInLoop: number = -1,
   loopLength: number = -1
-) {
-  // eslint-disable-next-line no-param-reassign
-  tpl = tpl.replace(
-    /{{for ([^\}]+)}}((?:\s|\S)+){{endfor}}/gi, // eslint-disable-line no-useless-escape
-    (match, param, body) => {
-      if (data[param]) {
-        return data[param]
-          .map((el: string, index: number) =>
-            internal(body, el, index, data[param].length)
-          )
-          .join("");
-      }
-      return "";
-    }
-  );
-
-  // eslint-disable-next-line no-param-reassign
-  tpl = tpl.replace(
-    /{{if ([^\}]+)}}((?:\s|\S)+){{endif}}/gi, // eslint-disable-line no-useless-escape
-    (match, param, body) => {
-      const arr: string[] = body.split("{{else}}");
-      if (param in data) {
-        if (data[param]) {
-          return arr[0];
-        }
-        if (arr.length === 2) {
-          return arr[1];
-        }
-        return "";
-      } else if (param === "notIsLastElement") {
-        if (indexInLoop !== loopLength - 1) {
-          return arr[0];
-        } else if (arr.length === 2) {
-          return arr[1];
-        }
-        return "";
-      } else if (param === "isLastElement") {
-        if (indexInLoop === loopLength - 1) {
-          return arr[0];
-        } else if (arr.length === 2) {
-          return arr[1];
-        }
-        return "";
-      }
-      return "";
-    }
-  );
-
-  // eslint-disable-next-line no-useless-escape
-  return tpl.replace(/{{([^\}]+)}}/gi, (match, param) => {
-    return data[param] ? data[param] : "";
-  });
+): string {
+  let replaced = loopTemplate(tpl, data);
+  replaced = ifTemplate(replaced, data, indexInLoop, loopLength);
+  return keyTemplate(replaced, data);
 }
 
 export function template(tpl: string, data: any): string {
